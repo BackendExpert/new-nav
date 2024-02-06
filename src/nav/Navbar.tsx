7a4626aee2d22d2334abffed23a54e99f42d120e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 19:58:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -16,10 +16,13 @@ const navBar = () => {
             <IonIcon name={openMenu ? "close" : "menu"}/>
           </div>
         <div>
-          <ul className={`md:flex md:items-center md:pb-0 absolute md:static z-auto z-[-1] md:bg-gray-700 bg-gray-800 left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in ${openMenu ? 'top-20 ':'top-[-590px]'}`}>
+          <ul className={`md:flex md:items-center md:pb-0 absolute md:static z-auto z-[-1] md:bg-gray-700 bg-gray-800 left-0 md:mt-0 mt-[-16px] w-full md:w-auto md:pl-0 md:py-0 py-4 md:px-0 px-2 transition-all duration-500 ease-in ${openMenu ? 'top-20 ':'top-[-590px]'}`}>
             <li className="px-4 py-4 md:py-0"><a href="">Home</a></li>
             <li className="px-4 py-4 md:py-0">
               <span className="cursor-pointer flex" onClick={() => AboutOpen(!openAbout)}>About <span className="pl-2 pt-[0px]"><IonIcon name={openAbout ? "chevron-down" : "chevron-up"}/></span></span>
+              <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] left-12 md:w-11/12 md:mr-12 w-auto ml-0 pr-24 md:pl-12 pl-9 md:mt-[-14px] md:mr-10 mr-8 transition-all duration-500 ease-in ${openAbout ? 'top-20':'top-[-100px]'}`}>
+                  asdasd
+              </div>
             </li>
             <li className="px-4 py-4 md:py-0"><a href="">Home</a></li>
             <li className="px-4 py-4 md:py-0"><a href="">Home</a></li>
