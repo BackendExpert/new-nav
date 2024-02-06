c5ed1002763df49413e5da79fa94ede7ae63aab9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 18:49:23 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -4,7 +4,7 @@ const navBar = () => {
   return (
     <div className="shadow-md w-full top-0 left-0 bg-blue-900">
       <div className="md:flex items-center justify-between bg-gray-700 text-white py-4 md:px-20 px-7">
-        <IonIcon name="menu" /> 
+      <IonIcon name="chatbubbles" />
       </div>
     </div>
   )
