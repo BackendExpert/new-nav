a283930ddbabd9e9bba65447bc610ae0275a603e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 19:16:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,31 @@ import NavBar from "./nav/Navbar"
 function App() {
 
   return (
-      <NavBar />
+      <div>
+        <NavBar />   
+        <div className="px-4 py-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem inventore eum enim beatae amet id harum quaerat dolore voluptate reiciendis ut aut rerum, fuga laborum expedita, eius assumenda libero labore.
+          Adipisci autem nisi cum eum consequatur, non ipsa expedita! Fugiat velit, explicabo, assumenda culpa molestias dicta aliquid reprehenderit natus magni alias eveniet veniam laborum autem praesentium nemo, dolor tempore harum!
+          Culpa odio non eligendi sequi unde. Velit soluta vel eius rem non sequi laudantium voluptatem est natus praesentium earum, pariatur cum dolor numquam qui temporibus quis facere optio dolorem! Accusantium.
+          Esse placeat itaque perferendis incidunt nam ducimus fuga perspiciatis libero voluptas facere exercitationem aspernatur, repudiandae numquam nihil nostrum accusamus cumque laboriosam eius quos assumenda. Voluptatum incidunt consequuntur ex rerum ea.
+          Repellat fugit iure voluptas inventore quo pariatur quidem nulla tempore excepturi voluptatem atque adipisci consequuntur est consectetur ratione officia vitae fuga delectus aperiam, similique quis alias cupiditate nisi tempora. Repellat!
+          Debitis facere molestiae, neque excepturi at ullam reprehenderit eveniet eum nisi quaerat? Doloremque, odit. Quibusdam debitis eius impedit pariatur atque quae, officia tempora sapiente consequatur voluptas vel unde quasi reiciendis.
+          Pariatur veritatis doloribus modi soluta praesentium expedita eveniet repudiandae voluptate quis quisquam ut quia rem totam, beatae ducimus animi autem dignissimos unde perferendis. Atque odio ullam laboriosam id incidunt placeat.
+          Aliquam distinctio, vel possimus ex ab corporis voluptatibus magni ea quae vitae nihil libero assumenda quod, nulla adipisci odio eius. Accusantium minus ut illo eveniet facilis ipsa voluptates nostrum inventore!
+          Doloremque dolor perspiciatis amet, sunt magni eos, reprehenderit quis, maxime qui fugiat ex porro consectetur sint ab animi rem nisi. Dolor dicta ipsum architecto nemo velit tempore consectetur minima veniam?
+          Hic voluptatum delectus a accusantium laborum consequuntur quasi ipsam, eligendi in provident dicta, quam quisquam sed magnam ea temporibus ad? Rem sed aliquam, quae et veniam dolor maiores laborum laboriosam.
+          Laudantium sit, praesentium dolor dicta saepe aliquid at doloribus incidunt libero perferendis maiores commodi tempora, iure porro consequatur quos! Quae, itaque? Quam odit culpa dolore est placeat nesciunt, iure officiis.
+          Odio repellendus illum perspiciatis eligendi reprehenderit optio nihil facere assumenda consequatur magnam dolor autem nesciunt, adipisci, quibusdam architecto perferendis dolorem harum possimus. Quae voluptates explicabo ullam nemo maxime quos sed.
+          Aliquam, aperiam nemo deserunt dignissimos corrupti eius laudantium, nam similique doloribus perspiciatis iste itaque quae expedita nobis architecto saepe, alias ipsa repellendus veniam optio fuga rem mollitia dolores. Optio, reprehenderit!
+          Modi aliquam nihil libero. Aliquam, eius nihil dicta odit minus neque porro magni! Possimus, alias aperiam officiis optio nam ipsum, ut recusandae praesentium fugit sapiente suscipit, quos fugiat ipsa repellendus.
+          Est fuga qui doloribus quaerat sunt consequuntur aliquid non dignissimos molestias sapiente quibusdam magnam officiis laborum pariatur illum ratione dolorem, nobis iste voluptates quisquam! Corporis molestiae nemo molestias quo similique.
+          Aperiam, quasi? Aspernatur modi et odit blanditiis beatae exercitationem odio, possimus repellat suscipit sapiente ipsa maxime laudantium alias? Omnis nostrum iure sunt, laborum fugit inventore eligendi. Est velit eum dolorum?
+          Facilis porro nihil ab. Quisquam fuga quasi deserunt aut, itaque voluptatibus est possimus minima, error illo quam expedita atque odio vero quae neque. Expedita quo rerum nulla, sit impedit perspiciatis!
+          Alias quia molestiae fugiat accusantium tempore! Sed iste qui at facilis eius velit unde, adipisci esse beatae dignissimos iure illo, amet maiores quae eveniet eligendi? Animi, laboriosam? Doloremque, architecto aspernatur.
+          Eligendi odit ipsam optio reprehenderit deserunt, maxime ipsum provident illum possimus dolorum nulla, odio corporis sit recusandae eveniet expedita eum blanditiis adipisci ut suscipit. Vitae ipsa aperiam aut vel ea!
+          Magni quae magnam architecto. Odio cupiditate temporibus nesciunt! Error doloribus odit soluta debitis facere quia, ut veniam repellat ex doloremque neque rerum corporis blanditiis itaque quaerat velit modi tempora numquam.  
+        </div>     
+      </div>
   )
 }
 
--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -12,14 +12,14 @@ const navBar = () => {
           <span className="text-xl px-2">Logo Here</span>
         </span>
         <div>
-          <ul className="flex absolute">
-            <li className="px-4"><a href="">Home</a></li>
-            <li className="px-4">
+          <ul className="md:flex md:item-center md:pb-0 pb-6 pt-4 pl-4">
+            <li className="px-4 py-4"><a href="">Home</a></li>
+            <li className="px-4 py-4">
               <span className="cursor-pointer flex" onClick={() => AboutOpen(!openAbout)}>About <span className="pl-2 pt-[2px]"><IonIcon name={openAbout ? "chevron-down" : "chevron-up"}/></span></span>
             </li>
-            <li className="px-4"><a href="">Home</a></li>
-            <li className="px-4"><a href="">Home</a></li>
-            <li className="px-4"><a href="">Home</a></li>
+            <li className="px-4 py-4"><a href="">Home</a></li>
+            <li className="px-4 py-4"><a href="">Home</a></li>
+            <li className="px-4 py-4"><a href="">Home</a></li>
           </ul>
         </div>
       </div>
