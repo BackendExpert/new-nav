5ce864d2563d44672ac7d80129374776fe255fd8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 20:56:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -62,10 +62,6 @@ const navBar = () => {
   ];
 
 
-
-
-
-
   return (
     <div className="shadow-md w-full top-0 left-0 bg-blue-900">
       <div className="md:flex items-center justify-between bg-gray-700 text-white py-4 md:px-20 px-7">
