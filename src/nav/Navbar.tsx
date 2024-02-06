1039bc109e44d6043b5a24f8be8773d35e094dc5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 21:23:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -161,7 +161,7 @@ const navBar = () => {
             <li className="px-4 py-4 md:py-0"><a href="">News & Event</a></li>
             <li className="px-4 py-4 md:py-0">
               <span className="cursor-pointer flex" onClick={() => AcadamyOpen(!openAcadamy)}>Academic <span className="pl-2 pt-[0px]"><IonIcon name={openAcadamy ? "chevron-up" : "chevron-down"}/></span></span>
-              <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] md:w-11/12 w-11/12 md:left-12 left-[-2px] ml-4 md:mr-12 mt-[145px] w-auto ml-0 pr-24 md:pl-12 pl-9 md:mt-[-14px] md:mr-10 mr-8 transition-all duration-500 ease-in ${openAcadamy ? 'top-20':'md:top-[-3500px] top-[-2000px]'}`}>
+              <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] md:w-11/12 w-11/12 md:left-12 left-[-2px] ml-4 md:mr-12 mt-[145px] w-auto ml-0 pr-24 md:pl-12 pl-9 md:mt-[-14px] md:mr-10 mr-8 transition-all duration-500 ease-in ${openAcadamy ? 'top-20':'md:top-[-320px] top-[-2000px]'}`}>
                 <div className="md:grid md:grid-cols-2 gap-2 md:px-12 px-0 py-4">  
 
                     <div>
@@ -195,7 +195,7 @@ const navBar = () => {
             </li>
             <li className="px-4 py-4 md:py-0">
               <span className="cursor-pointer flex" onClick={() => PubOpen(!openpub)}>Publication <span className="pl-2 pt-[0px]"><IonIcon name={openpub ? "chevron-up" : "chevron-down"}/></span></span>
-              <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] md:w-11/12 w-11/12 md:left-12 left-[-2px] ml-4 md:mr-12 mt-[200px] w-auto ml-0 pr-24 md:pl-12 pl-9 md:mt-[-14px] md:mr-10 mr-8 transition-all duration-500 ease-in ${openpub ? 'top-20':'md:top-[-3500px] top-[-2000px]'}`}>
+              <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] md:w-11/12 w-11/12 md:left-12 left-[-2px] ml-4 md:mr-12 mt-[200px] w-auto ml-0 pr-24 md:pl-12 pl-9 md:mt-[-14px] md:mr-10 mr-8 transition-all duration-500 ease-in ${openpub ? 'top-20':'md:top-[-350px] top-[-2000px]'}`}>
                 <div className="md:grid md:grid-cols-3 gap-2 md:px-12 px-0 py-4">  
                     <div>
                       <span className="text-2xl"><a href="">YSCMR</a></span>
