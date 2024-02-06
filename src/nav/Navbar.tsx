546f598e62cf69eb783c3c0cf64183672327d440
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 19:32:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -16,7 +16,7 @@ const navBar = () => {
             <IonIcon name={openMenu ? "close" : "menu"}/>
           </div>
         <div>
-          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${openMenu ? 'top-20 ':'top-[-490px]'}`}>
+          <ul className={`md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-[-1]`}>
             <li className="px-4 py-4 md:py-0"><a href="">Home</a></li>
             <li className="px-4 py-4 md:py-0">
               <span className="cursor-pointer flex" onClick={() => AboutOpen(!openAbout)}>About <span className="pl-2 pt-[0px]"><IonIcon name={openAbout ? "chevron-down" : "chevron-up"}/></span></span>
