59c2c86a74ea149b9c43d3a7292fc63de2b27329
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-06 21:06:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/nav/Navbar.tsx
+++ src/nav/Navbar.tsx
@@ -193,7 +193,7 @@ const navBar = () => {
               </div>
             </li>
             <li className="px-4 py-4 md:py-0">
-              <span className="cursor-pointer flex" onClick={() => PubOpen(!openpub)}>Academic <span className="pl-2 pt-[0px]"><IonIcon name={openpub ? "chevron-up" : "chevron-down"}/></span></span>
+              <span className="cursor-pointer flex" onClick={() => PubOpen(!openpub)}>Publication <span className="pl-2 pt-[0px]"><IonIcon name={openpub ? "chevron-up" : "chevron-down"}/></span></span>
               <div className={`rounded items-center py-6 px-4 absolute static bg-gray-700  z-auto z-[-1] md:w-11/12 w-11/12 md:left-12 left-[-2px] ml-4 md:mr-12 mt-[145px] w-auto ml-0 pr-24 md:pl-12 pl-9 md:mt-[-14px] md:mr-10 mr-8 transition-all duration-500 ease-in ${openpub ? 'top-20':'md:top-[-3500px] top-[-2000px]'}`}>
                 <div className="md:grid md:grid-cols-3 gap-2 md:px-12 px-0 py-4">  
 
